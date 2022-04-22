99cf74e711a12b18da0fdb3e34b376fae3bbd8ba
enrique1994mex <enrique.antonio1029@gmail.com>
2022-04-22 11:37:50 -0500

    Updates

---
+++ precache-manifest.0ec5711c334f28f5a8baf60a9ae86718.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bc883276cecfaa94edc983ab763886",
+    "url": "/prueba-shopping/index.html"
+  },
+  {
+    "revision": "204ffe2382385db13fac",
+    "url": "/prueba-shopping/static/css/2.7300059f.chunk.css"
+  },
+  {
+    "revision": "aed9a518eb877164b933",
+    "url": "/prueba-shopping/static/css/main.4a8c8305.chunk.css"
+  },
+  {
+    "revision": "204ffe2382385db13fac",
+    "url": "/prueba-shopping/static/js/2.885e426b.chunk.js"
+  },
+  {
+    "revision": "00c87f11ef0f3d3505371bc19c173d0e",
+    "url": "/prueba-shopping/static/js/2.885e426b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aed9a518eb877164b933",
+    "url": "/prueba-shopping/static/js/main.51aed685.chunk.js"
+  },
+  {
+    "revision": "b0ef79199264f85e7942",
+    "url": "/prueba-shopping/static/js/runtime-main.d6798d2e.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/prueba-shopping/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/prueba-shopping/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/prueba-shopping/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/prueba-shopping/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/prueba-shopping/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
